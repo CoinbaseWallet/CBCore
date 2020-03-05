8dd4760dec53245ea01d4ce3950ab23bf7d195a0
alexshoykhet <alex.shoykhet@coinbase.com>
2020-03-05 11:00:22 -0800

    Add BoundedCache to core library, also add method for safely casting map params (#34)
    * Add BoundedCache to core library, also add method for safely casting Map parameters to specific types
    
    * Run ktlint format
    
    * Remove unnecessary BoundedCache.  ConcurrentLruCache is sufficient

--- android/core/src/main/java/com/coinbase/wallet/core/extensions/Map+Core.kt
+++ android/core/src/main/java/com/coinbase/wallet/core/extensions/Map+Core.kt
@@ -8,3 +8,17 @@ package com.coinbase.wallet.core.extensions
  * @return the non-null value of type V
  */
 fun <K, V> Map<K, V>.require(key: K): V = get(key) ?: throw NoSuchElementException("$key in map is missing.")
+
+/**
+ * Make sure map keys and values are all the correct type
+ */
+inline fun <reified K, reified V> Map<*, *>?.asMapOfType(): Map<K, V>? {
+    if (this == null) return null
+
+    return if (all { (key, value) -> key is K && value is V }) {
+        @Suppress("UNCHECKED_CAST")
+        this as Map<K, V>
+    } else {
+        null
+    }
+}
